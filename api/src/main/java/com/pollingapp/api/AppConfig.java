34b1e5ae3d8a5a908f55d1232271ca84dfad79ff
Oleksandr Tovshchyk <157808651+takiido@users.noreply.github.com>
2025-01-28 20:11:57 -0600

    created app config

--- api/src/main/java/com/pollingapp/api/AppConfig.java
+++ api/src/main/java/com/pollingapp/api/AppConfig.java
@@ -1,6 +1,9 @@
-package com.pollingapp.api.poll;
+package com.pollingapp.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.pollingapp.api.option.Option;
+import com.pollingapp.api.option.OptionService;
+import com.pollingapp.api.poll.Poll;
+import com.pollingapp.api.poll.PollService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,35 +11,46 @@ import org.springframework.context.annotation.Configuration;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static java.time.Month.*;
+import static java.time.Month.JANUARY;
 
 @Configuration
-public class PollConfig {
+public class AppConfig {
 
     PollService pollService;
+    OptionService optionService;
 
-    @Autowired
-    public PollConfig(PollService pollService) {
+    public AppConfig(PollService pollService, OptionService optionService) {
         this.pollService = pollService;
+        this.optionService = optionService;
     }
 
     @Bean
-    CommandLineRunner runner(PollRepository repository) {
+    CommandLineRunner runner(PollService pollService, OptionService optionService) {
         return args -> {
             Poll poll = new Poll(
                     "Test poll",
                     "Test poll description",
                     LocalDateTime.of(2025, JANUARY, 28, 12, 30, 0)
             );
-
             Poll poll2 = new Poll(
                     "Test poll 2",
                     "Test poll 2 description",
                     LocalDateTime.of(2025, JANUARY, 28, 12, 30, 0)
             );
 
+            Option option1p1 = new Option(
+                    "Poll 1 option 1",
+                    0,
+                    poll
+            );
+
+            List<Option> options = List.of(option1p1);
+
+            poll.setOptions(options);
+
             pollService.createPoll(poll);
             pollService.createPoll(poll2);
+            optionService.createOption(option1p1);
         };
     }
 }
--- api/src/main/java/com/pollingapp/api/option/OptionController.java
+++ api/src/main/java/com/pollingapp/api/option/OptionController.java
@@ -1,6 +1,7 @@
 package com.pollingapp.api.option;
 
 import com.pollingapp.api.poll.Poll;
+import com.pollingapp.api.poll.PollService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,31 +11,34 @@ import java.util.List;
 public class OptionController {
 
     private final OptionService optionService;
+    private final PollService pollService;
 
     @Autowired
-    public OptionController(OptionService optionService) {
+    public OptionController(OptionService optionService, PollService pollService) {
         this.optionService = optionService;
+        this.pollService = pollService;
     }
 
-    @RequestMapping(path="api/v1/polloptions/{id}")
+    @RequestMapping(path="api/v1/options/{id}", method = RequestMethod.GET)
     @GetMapping
-    public List<Option> getOptionsByPoll(Poll poll) {
-        return this.optionService.getAllOptions(poll);
+    public List<Option> getOptionsByPoll(@PathVariable long id) {
+        Poll poll = pollService.getPollById(id);
+        return optionService.getAllOptions(poll);
     }
 
-    @RequestMapping(path="api/v1/option")
+    @RequestMapping(path="api/v1/option", method = RequestMethod.POST)
     @PostMapping
     public void createOption(@RequestBody Option option) {
-        this.optionService.createOption(option);
+        optionService.createOption(option);
     }
 
-    @RequestMapping(path="api/v1/option/{id}")
+    @RequestMapping(path="api/v1/option/{id}", method = RequestMethod.DELETE)
     @DeleteMapping
     public void deleteOption(@PathVariable(value = "id") Long id) {
         optionService.deleteOption(id);
     }
 
-    @RequestMapping(path = "api/v1/option/{id}")
+    @RequestMapping(path = "api/v1/option/{id}", method = RequestMethod.PATCH)
     @PatchMapping
     public void PatchOption(@PathVariable(value = "id") Long id, @RequestBody Option option) {
         optionService.patchOption(id, option);
